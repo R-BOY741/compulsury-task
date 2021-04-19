36533e77abb71784a5306135a8a5782d11f6e7f6
R-BOY741 <82158973+R-BOY741@users.noreply.github.com>
2021-04-19 11:02:36 +0200

    Add files via upload

---
+++ compulsury-task.py
@@ -0,0 +1,12 @@
+# Deretermine the speed
+      alllowed_speed float(input("What was the allowed speed on the road: "))
+        current_speed float(input("what is your current speed: "))
+        diff//5 allowed_spped-currentspeed
+                points=2
+      allowed_speed = 60:
+    if road_average_speed =< 60:
+        print("Go to jail")
+
+
+
+
