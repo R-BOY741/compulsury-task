04262ccb35379a6f91c6637d607d2c37b1d6a09c
R-BOY741 <82158973+R-BOY741@users.noreply.github.com>
2021-04-19 11:18:36 +0200

    Update compulsury-task.py

--- compulsury-task.py
+++ compulsury-task.py
@@ -1,6 +1,6 @@
 # Deretermine the speed
-      alllowed_speed float(input("What was the allowed speed on the road: "))
-        current_speed float(input("what is your current speed: "))
+      alllowed_speed float(input("What was the allowed speed on the road": ))
+        current_speed float(input("what is your current speed": ))
         diff//5 allowed_spped-currentspeed
                 points=2
       allowed_speed = 60:
@@ -8,5 +8,7 @@
         print("Go to jail")
 
 
+            
+  
 
 
